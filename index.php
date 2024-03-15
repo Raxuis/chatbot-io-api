b870687c398fe72710cad561c3f3d8d640cb85b6
BenoitPrmt <benoitparmentier45@gmail.com>
2024-03-15 10:02:33 +0100

    feat(api): get args from url

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 require 'vendor/autoload.php';
 
+echo "Structure de l'URL : localhost:8080/v1/user/id";
 
+// $_SERVER['REQUEST_METHOD']
 
-echo "Hello World!<br>";
+$uriArgs = array_slice(explode("/", $_SERVER['REQUEST_URI']), 1);
+$apiVersion = $uriArgs[0];
+$apiRoute = $uriArgs[1];
+$apiArgs = array_slice($uriArgs, 2);
 
-echo $_SERVER['REQUEST_URI'] . "<br>";
-echo $_SERVER['REQUEST_METHOD'];
-
-
+echo "<pre><br>";
+echo "API Version: " . $apiVersion . "<br>";
+echo "Route: " . $apiRoute . "<br>";
+var_dump($apiArgs);
+echo "</pre>";
