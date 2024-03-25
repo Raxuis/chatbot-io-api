82b6871abf3b1149bde2c004e65b7aed1bafad25
Raxuis <raclot.raphael@gmail.com>
2024-03-25 10:35:26 +0100

    test(User class): :white_check_mark: Add json to run function

--- src/controllers/User.php
+++ src/controllers/User.php
@@ -2,17 +2,26 @@
 
 namespace App\Controllers;
 
-class User {
+class User
+{
 
   protected array $params;
 
-  public function __construct($params) {
+  public function __construct($params)
+  {
     $this->params = $params;
     $this->run();
   }
 
   protected function run(): void
   {
-    echo json_encode($this->params);
+    echo json_encode(
+      [
+        'id' => 1,
+        'first_name' => "Elliot",
+        'last_name' => "Alderson",
+        'email' => "huh@gmail.com",
+      ]
+    );
   }
 }
