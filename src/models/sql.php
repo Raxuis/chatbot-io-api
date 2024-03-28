cef3ea331973d51349ae8560c830f4ec03f71a77
BenoitPrmt <benoitparmentier45@gmail.com>
2024-03-28 10:45:25 +0100

    fix: type exept

--- src/models/sql.php
+++ src/models/sql.php
@@ -10,12 +10,8 @@ class sql {
 
   public function __construct(array $config) {
     $this->host = 'mysql:host='.$config['host'].';port='.$config['port'].';dbname='.$config['dbname'].';charset=utf8';
-
-    try {
-      $this->db = new PDO($this->host, $config['user'], $config['password']);
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
+    
+    $this->db = new PDO($this->host, $config['user'], $config['password']);
 
     $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
