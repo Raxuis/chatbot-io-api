5d92ef788480a0e45ba11c574c75246c76e3eba8
BenoitPrmt <benoitparmentier45@gmail.com>
2024-04-04 09:06:26 +0200

    feat(db): added new database model

--- database/chatbot_io.sql
+++ database/chatbot_io.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : mer. 03 avr. 2024 à 15:52
+-- Généré le : jeu. 04 avr. 2024 à 07:05
 -- Version du serveur : 5.7.39
--- Version de PHP : 7.4.33
+-- Version de PHP : 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,21 @@ CREATE TABLE `bots` (
   `actions` set('helloAction','pokemonAction','serpentAction','parkingAction','swAction','headsOrTailsAction','shifumiAction','passwordAction','weatherAction','loveAction','fortniteShopAction','fortniteLastAddedAction','fortniteMapAction','fortniteStatsAction','exchangeRateAction','valorantAgentsAction','movieAction','trendingAction') NOT NULL DEFAULT 'helloAction'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `bots`
+--
+
+INSERT INTO `bots` (`id`, `name`, `description`, `avatar`, `actions`) VALUES
+(1, 'Assistant', 'I am an assistant to help you !', 'https://source.boringavatars.com/', 'helloAction,parkingAction,passwordAction,exchangeRateAction'),
+(2, 'Evelyne', 'Miss météo', 'https://www.gala.fr/imgre/fit/~1~gal~2023~10~02~c259200d-c07b-45a2-94bc-b2b9c289807b.jpeg/2671x1914/quality/80/evelyne-dheliat.jpeg', 'helloAction,weatherAction'),
+(3, 'Professor Oak', 'Catch them all', 'https://d1lss44hh2trtw.cloudfront.net/resize?type=webp&url=https%3A%2F%2Fshacknews-www.s3.amazonaws.com%2Fassets%2Feditorial%2F2018%2F11%2Fprofoakbig.jpg&width=986&sign=ZvLPW1bobNJKiK0uRyfilVYVkukVqFJGb-zCwkYo-kg', 'helloAction,pokemonAction'),
+(4, 'Game Master', 'I am the game master', 'https://img.freepik.com/free-photo/friends-having-fun-while-playing-poker_23-2149273957.jpg', 'helloAction,headsOrTailsAction,shifumiAction'),
+(5, 'C-3PO', 'A pleasure to meet you. I am C-3PO, Human-Cyborg Relations.', 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/2800/C3PO.Star-Wars-Series.webp', 'helloAction,swAction'),
+(6, 'Woody', 'There\'s a snake in his boot', 'https://www.sosyncd.com/wp-content/uploads/2022/07/87.png', 'helloAction,serpentAction'),
+(7, 'Cupid', 'I calculate your love percentage', 'https://media.istockphoto.com/id/637985650/vector/cute-little-cupid-aiming-at-someone.jpg?s=612x612&w=0&k=20&c=o1Gi4i_kRZKzYKaA73zAjel9F0mNIEOnTKjvGOjZnbs=', 'helloAction,loveAction'),
+(8, 'Video Game', 'I can help you in every game', 'https://www.pngitem.com/pimgs/m/146-1468323_gamer-profile-icon-png-transparent-png.png', 'helloAction,fortniteShopAction,fortniteLastAddedAction,fortniteMapAction,fortniteStatsAction,valorantAgentsAction'),
+(9, 'Cinema', 'Search movies', 'https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 'helloAction,movieAction,trendingAction');
+
 -- --------------------------------------------------------
 
 --
@@ -61,6 +76,13 @@ CREATE TABLE `users` (
   `name` varchar(30) NOT NULL,
   `avatar` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `avatar`) VALUES
+(1, 'Elliot', 'https://i.pinimg.com/736x/a3/b3/1a/a3b31a3d62d7643ebd97c49dc8c43ffa.jpg');
 
 --
 -- Index pour les tables déchargées
@@ -94,7 +116,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `bots`
 --
 ALTER TABLE `bots`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `messages`
@@ -106,7 +128,7 @@ ALTER TABLE `messages`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Contraintes pour les tables déchargées
